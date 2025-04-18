7568d414c31ec0142edf6719dcec2b27070b9ac2
Alejandro Nuñez <ale.nu.rdz@gmail.com>
2025-04-18 01:26:33 -0600

    formulario registrar user

--- usuarios/models.py
+++ usuarios/models.py
@@ -11,7 +11,8 @@ class Usuario(AbstractUser):
     rol = models.CharField(max_length=10, choices=ROL_CHOICES, default='BASICO')
     telefono = models.CharField(max_length=20, blank=True)
     direccion = models.CharField(max_length=255, blank=True)
-    dni = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    primer_apellido = models.CharField(max_length=50)
+    segundo_apellido = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.username} ({self.get_rol_display()})"
+        return f"{self.first_name} {self.primer_apellido} {self.segundo_apellido} ({self.get_rol_display()})"
