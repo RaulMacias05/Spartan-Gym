8d0350e346010d49e0c16bd7f1defff5230a69ee
Raúl Macías <raul.sanchezm@uanl.edu.mx>
2025-04-26 16:46:59 -0600

    Se arreglan pequeños errores

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from ventas.models import Venta
 
 # Create your views here.
 def signin(request):
@@ -22,6 +25,10 @@ def signin(request):
             return redirect('core:inicio')
 
 def inicio(request):
-    return render(request, 'core/inicio.html')
+    ventas = Venta.objects.all()
+    ventas_hoy = sum(1 for i in ventas.filter(fecha_venta__date=datetime.today().date()))
+    return render(request, 'core/inicio.html', {
+        'ventas_hoy': ventas_hoy
+    })
 
 
